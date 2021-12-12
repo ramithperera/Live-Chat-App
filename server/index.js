ad92092926099096d6763100f6e7ccb046394a80
Ramith <89182652+ramithperera@users.noreply.github.com>
2021-12-12 21:24:20 +0530

    foundation

--- server/index.js
+++ server/index.js
@@ -9,21 +9,27 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined the room: ${data}`);
+  });
+  socket.on("send_message", (data) => {
+    console.log(data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
 server.listen(3001, () => {
-    console.log("SERVER RUNNING");
-})
+  console.log("SERVER RUNNING");
+});
